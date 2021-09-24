2cb835c401636dc3b04e6128eaa067fd97468257
Leejha <57664427+Leejha@users.noreply.github.com>
2021-09-24 10:40:30 +0000

    fix: Add display grid

--- client/src/components/meet/MeetGridItem.tsx
+++ client/src/components/meet/MeetGridItem.tsx
@@ -1,17 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import Styled from "styled-components";
 
-<<<<<<< HEAD
-=======
-const VideoContainer = Styled.video<{ ItemWidth: number; ItemHeight: number }>`
-    background-color: black;
-    object-fit: cover; // video 태그 무조건 1:1 비율인걸 깨뜨려주는 얘임
-    width: ${(props) => props.ItemWidth}px;
-    height: ${(props) => props.ItemHeight}px;
-`;
-
->>>>>>> 1784f4249506dee64ffc0509966687bcb73c1ebd
 interface Props {
   stream: MediaStream;
   muted?: boolean;
@@ -24,24 +13,15 @@ const MeetGridItem = ({ stream, muted, width, isLastRow }: Props) => {
   const [isMuted, setIsMuted] = useState<boolean>(false);
 
   useEffect(() => {
+    console.log("width", width);
     if (ref.current) ref.current.srcObject = stream;
     if (muted) setIsMuted(muted);
   }, [muted, stream]);
 
   return (
-<<<<<<< HEAD
-    <MeetGridItemBlock style={{ width, flex: isLastRow ? 1 : undefined }}>
+    <MeetGridItemBlock style={{ width }}>
       <VideoContainer ref={ref} muted={isMuted} autoPlay />
     </MeetGridItemBlock>
-=======
-    <VideoContainer
-      ref={ref}
-      muted={isMuted}
-      ItemWidth={width}
-      ItemHeight={height}
-      autoPlay
-    ></VideoContainer>
->>>>>>> 1784f4249506dee64ffc0509966687bcb73c1ebd
   );
 };
 
@@ -50,7 +30,7 @@ const MeetGridItemBlock = styled.div`
   position: relative;
 `;
 
-const VideoContainer = Styled.video`
+const VideoContainer = styled.video`
   object-fit: cover; // video 태그 무조건 1:1 비율인걸 깨뜨려주는 얘임
   position: absolute;
   left: 0;
--- client/src/pages/MeetPage.tsx
+++ client/src/pages/MeetPage.tsx
@@ -1,316 +1,56 @@
-import React, { useState } from "react";
-import io from "socket.io-client";
+import React, { useEffect } from "react";
+import { useLayoutEffect } from "react";
+import { useMemo } from "react";
 import { useRef } from "react";
-import { useEffect } from "react";
-import { useParams } from "react-router";
-import MeetGrid from "components/meet/MeetGrid";
+import { useState } from "react";
 import styled from "styled-components";
-import MeetFooter from "components/meet/MeetFooter";
-import Sidebar from "components/meet/MeetSideBar";
+import MeetGridItem from "components/meet/MeetGridItem";
 
-interface MeetParams {
-  roomId: string;
+interface MeetGridProps {
+  users: IWebRTCUser[];
+  sidebarOpen: boolean;
 }
 
-const Meet = () => {
-  const [socket, setSocket] = useState<SocketIOClient.Socket>();
-  const [users, setUsers] = useState<Array<IWebRTCUser>>([]);
+function MeetGrid({ users, sidebarOpen }: MeetGridProps) {
+  const [itemWidth, setItemWidth] = useState(0);
+  const ref = useRef<any>();
 
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const divisor = useMemo(() => {
+    return Math.ceil(Math.sqrt(users.length)) || 1;
+  }, [users.length]);
 
-  const { roomId } = useParams<MeetParams>();
-
-  let localVideoRef = useRef<HTMLVideoElement>(null);
-
-  let sendPC: RTCPeerConnection;
-  let receivePCs: { [socketId: string]: RTCPeerConnection };
-
-  const pc_config = {
-    iceServers: [
-      // {
-      //   urls: 'stun:[STUN_IP]:[PORT]',
-      //   'credentials': '[YOR CREDENTIALS]',
-      //   'username': '[USERNAME]'
-      // },
-      {
-        urls: "stun:stun.l.google.com:19302",
-      },
-    ],
-  };
-
-  useEffect(() => {
-    let newSocket = io.connect("http://localhost:8080");
-    let localStream: MediaStream;
-
-    newSocket.on("userEnter", (data: { id: string }) => {
-      createReceivePC(data.id, newSocket);
-    });
-
-    newSocket.on("allUsers", (data: { users: Array<{ id: string }> }) => {
-      let len = data.users.length;
-      for (let i = 0; i < len; i++) {
-        createReceivePC(data.users[i].id, newSocket);
-      }
-    });
-
-    newSocket.on("userExit", (data: { id: string }) => {
-      receivePCs[data.id].close();
-      delete receivePCs[data.id];
-      setUsers((users) => users.filter((user) => user.id !== data.id));
-    });
-
-    newSocket.on(
-      "getSenderAnswer",
-      async (data: { sdp: RTCSessionDescription }) => {
-        try {
-          console.log("get sender answer");
-          console.log(data.sdp);
-          await sendPC.setRemoteDescription(
-            new RTCSessionDescription(data.sdp)
-          );
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    );
-
-    newSocket.on(
-      "getSenderCandidate",
-      async (data: { candidate: RTCIceCandidateInit }) => {
-        try {
-          console.log("get sender candidate");
-          if (!data.candidate) return;
-          sendPC.addIceCandidate(new RTCIceCandidate(data.candidate));
-          console.log("candidate add success");
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    );
-
-    newSocket.on(
-      "getReceiverAnswer",
-      async (data: { id: string; sdp: RTCSessionDescription }) => {
-        try {
-          console.log(`get socketID(${data.id})'s answer`);
-          let pc: RTCPeerConnection = receivePCs[data.id];
-          await pc.setRemoteDescription(data.sdp);
-          console.log(`socketID(${data.id})'s set remote sdp success`);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    );
-
-    newSocket.on(
-      "getReceiverCandidate",
-      async (data: { id: string; candidate: RTCIceCandidateInit }) => {
-        try {
-          console.log(data);
-          console.log(`get socketID(${data.id})'s candidate`);
-          let pc: RTCPeerConnection = receivePCs[data.id];
-          if (!data.candidate) return;
-          pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-          console.log(`socketID(${data.id})'s candidate add success`);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    );
-
-    setSocket(newSocket);
-    console.log("newSocket", newSocket);
-
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-        video: {
-          width: 240,
-          // height: 240,
-        },
-      })
-      .then((stream) => {
-        if (localVideoRef.current) localVideoRef.current.srcObject = stream;
-
-        localStream = stream;
-
-        // stream 정보에 내 데이터도 추가
-        const myStream = {
-          id: "myId",
-          stream,
-        };
-        setUsers(users.concat(myStream));
-
-        sendPC = createSenderPeerConnection(newSocket, localStream);
-        createSenderOffer(newSocket);
-
-        newSocket.emit("joinRoom", {
-          id: newSocket.id,
-          roomId,
-        });
-      })
-      .catch((error) => {
-        console.log(`getUserMedia error: ${error}`);
-      });
-  }, []);
-
-  const createReceivePC = (id: string, newSocket: SocketIOClient.Socket) => {
-    try {
-      console.log(`socketID(${id}) user entered`);
-      let pc = createReceiverPeerConnection(id, newSocket);
-      createReceiverOffer(pc, newSocket, id);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const createSenderOffer = async (newSocket: SocketIOClient.Socket) => {
-    try {
-      let sdp = await sendPC.createOffer({
-        offerToReceiveAudio: false,
-        offerToReceiveVideo: false,
-      });
-      console.log("create sender offer success");
-      await sendPC.setLocalDescription(new RTCSessionDescription(sdp));
-
-      newSocket.emit("senderOffer", {
-        sdp,
-        senderSocketID: newSocket.id,
-        roomId,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const createReceiverOffer = async (
-    pc: RTCPeerConnection,
-    newSocket: SocketIOClient.Socket,
-    senderSocketID: string
-  ) => {
-    try {
-      let sdp = await pc.createOffer({
-        offerToReceiveAudio: true,
-        offerToReceiveVideo: true,
-      });
-      console.log("create receiver offer success");
-      await pc.setLocalDescription(new RTCSessionDescription(sdp));
-
-      newSocket.emit("receiverOffer", {
-        sdp,
-        receiverSocketID: newSocket.id,
-        senderSocketID,
-        roomId,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const createSenderPeerConnection = (
-    newSocket: SocketIOClient.Socket,
-    localStream: MediaStream
-  ): RTCPeerConnection => {
-    let pc = new RTCPeerConnection(pc_config);
-
-    pc.onicecandidate = (e) => {
-      if (e.candidate) {
-        console.log("sender PC onicecandidate");
-        newSocket.emit("senderCandidate", {
-          candidate: e.candidate,
-          senderSocketID: newSocket.id,
-        });
-      }
-    };
-
-    pc.oniceconnectionstatechange = (e) => {
-      console.log(e);
-    };
-
-    if (localStream) {
-      console.log("localstream add");
-      localStream.getTracks().forEach((track) => {
-        pc.addTrack(track, localStream);
-      });
-    } else {
-      console.log("no local stream");
-    }
-
-    // return pc
-    return pc;
-  };
-
-  const createReceiverPeerConnection = (
-    socketID: string,
-    newSocket: SocketIOClient.Socket
-  ): RTCPeerConnection => {
-    let pc = new RTCPeerConnection(pc_config);
-
-    // add pc to peerConnections object
-    receivePCs = { ...receivePCs, [socketID]: pc };
-
-    pc.onicecandidate = (e) => {
-      if (e.candidate) {
-        console.log("receiver PC onicecandidate");
-        newSocket.emit("receiverCandidate", {
-          candidate: e.candidate,
-          receiverSocketID: newSocket.id,
-          senderSocketID: socketID,
-        });
-      }
-    };
-
-    pc.oniceconnectionstatechange = (e) => {
-      console.log(e);
-    };
-
-    pc.ontrack = (e) => {
-      console.log("ontrack success");
-      setUsers((oldUsers) => oldUsers.filter((user) => user.id !== socketID));
-      setUsers((oldUsers) => [
-        ...oldUsers,
-        {
-          id: socketID,
-          stream: e.streams[0],
-        },
-      ]);
-    };
-
-    // return pc
-    return pc;
-  };
+  useLayoutEffect(() => {
+    const gridWidth = sidebarOpen
+      ? document.body.offsetWidth - 420
+      : document.body.offsetWidth - 100;
+    setItemWidth(gridWidth / divisor);
+  }, [divisor, sidebarOpen]);
 
   return (
-    <MeetPageBlock>
-      <Wrapper>
-        <main>
-          <MeetGrid users={users} sidebarOpen={sidebarOpen} />
-        </main>
-        <Sidebar visible={sidebarOpen} />
-      </Wrapper>
-      <MeetFooter />
-    </MeetPageBlock>
+    <Grid ref={ref}>
+      {users.map((user, index) => {
+        const remainder = users.length % divisor;
+        const isLastRow = index >= users.length - remainder;
+        return (
+          <MeetGridItem
+            key={index}
+            isLastRow={isLastRow}
+            stream={user.stream}
+            width={itemWidth}
+          />
+        );
+      })}
+    </Grid>
   );
-};
+}
 
-const MeetPageBlock = styled.div`
-  height: 100%;
-  background: #212121;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Wrapper = styled.div`
-  flex: 1;
+const Grid = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
-  main {
-    height: 100%;
-    position: relative;
-    flex: 1;
-    min-height: 0;
-    overflow: hidden;
-  }
+  background: black;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
-export default Meet;
+export default MeetGrid;
