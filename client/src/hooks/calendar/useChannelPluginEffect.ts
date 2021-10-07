3ebc44d03c35c60f2926ac03d9be3c713fefa4c1
최수용 <dpfprtus@gmail.com>
2021-10-07 13:30:48 +0000

    feat:Refactor: divide component

--- client/src/hooks/calendar/useChannelPluginEffect.ts
+++ client/src/hooks/calendar/useChannelPluginEffect.ts
@@ -1,14 +1,19 @@
+
 import { useEffect, useState } from "react";
 import { readScheduleLinstAPI } from "lib/api/claendar";
 
 export default function useCalendarEffect() {
   const [schedules, setSchedules] = useState([]);
+
+
+
   useEffect(() => {
     const getData = async () => {
       const schedules = await readScheduleLinstAPI();
       setSchedules(schedules);
     };
-    getData();  
-  }, []);
+    getData();
+  }, []); //나중에 조건 달거임.
+
   return { schedules };
-}//나중에 조건 달거임.
+}
