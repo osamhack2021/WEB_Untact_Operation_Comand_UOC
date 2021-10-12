ae088a76e63977e7142cbcd61c2232159d92b063
Leejha <57664427+Leejha@users.noreply.github.com>
2021-10-12 13:17:43 +0000

    feat:Add chats button

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -1,3 +1,4 @@
+
 export { ReactComponent as CallIcon } from "./call.svg";
 export { ReactComponent as MicOffIcon } from "./mic_off.svg";
 export { ReactComponent as MicIcon } from "./mic.svg";
@@ -7,3 +8,4 @@ export { ReactComponent as PeopleIcon } from "./people.svg";
 export { ReactComponent as CloseIcon } from "./close.svg";
 export { ReactComponent as CallEndIcon } from "./call_end.svg";
 export { ReactComponent as ScreenShareIcon } from "./screen_share.svg";
+export { ReactComponent as ChatsIcon } from "./chats.svg";
